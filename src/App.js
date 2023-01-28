dd94a85103614ccf45e86c248fc8b3ef50117faa
likeam <92205472+likeam@users.noreply.github.com>
2023-01-28 15:14:51 +0500

    Game state building

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import Menu from "./components/Menu";
+import "./App.css";
+import { useState } from "react";
 
 
+// [menu, playing, finsished]
 function App() {
 
-
+  const [gameStae, setGameState] = useState('menu');
   return (
-    <div>
-        <h1>Hello React</h1>
+    <div className="App">
+        <h1>Quizz App</h1>
+        {gameStae === "menu" && <Menu />}
+        
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Menu = () => {
+    return (
+        <div className='Menu'>
+            <label >Enter Your Name</label>
+            <input type="text" placeholder='Ex. John Smith' />
+            <button>Start Quiz</button>
+        </div>
+    )
+}
+
+export default Menu
